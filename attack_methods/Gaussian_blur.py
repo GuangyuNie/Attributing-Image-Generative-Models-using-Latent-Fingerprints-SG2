f7b6ab3be95e26be0d3ef577a469894e65c24718
Guangyu(Jeremy) Nie <jeremy550959742@yahoo.com>
2022-11-08 11:07:24 -0700

    Update Gaussian_blur.py

--- attack_methods/Gaussian_blur.py
+++ attack_methods/Gaussian_blur.py
@@ -9,10 +9,10 @@ device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
 ##Uploading test
 class Gaussian_blur(nn.Module):
-    def __init__(self,kernel_size, is_train):
+    def __init__(self,sigma, is_train):
         super(Gaussian_blur, self).__init__()
 
-        self.sigma = np.array(kernel_size) / 3.0
+        self.sigma = np.array(sigma)
         self.filter_size = [3,9,15,19,25] #This filter size is calculated based on scipy source.
         if (not is_train):
             self.sigma = [self.sigma[-1]]
