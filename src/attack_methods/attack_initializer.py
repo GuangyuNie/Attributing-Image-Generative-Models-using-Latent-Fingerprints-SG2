3b7d1cb267ec5373aded30d6d53b6cdd6402dc05
Guangyu <gnie1@asu.edu>
2022-11-10 17:16:37 -0700

    debug attack method

--- src/attack_methods/attack_initializer.py
+++ src/attack_methods/attack_initializer.py
@@ -1,6 +1,6 @@
 from .Gaussian_blur import Gaussian_blur
 from .Gaussian_noise import Gaussian_noise
-#from .Jpeg_compression import JpegCompression
+from .Jpeg_compression import Jpeg
 from .DiffJPEG_master.DiffJPEG import DiffJPEG
 from .Combination import Combination_attack
 import torch
@@ -17,13 +17,12 @@ def attack_initializer(attack_method, is_train):
     elif (attack_method == 'Blur'):
         attack = Gaussian_blur(sigma=[opt.blur_sigma], is_train = is_train)
     elif(attack_method == "Jpeg"):
-        device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-        attack = DiffJPEG(height=opt.img_size, width=opt.img_size, differentiable=is_train, quality=opt.jpeg_quality).to(device)
+        attack = Jpeg(is_train, opt.jpeg_quality, opt.img_size).to(opt.device)
     elif (attack_method == 'Combination'):
         attacks = []
         attacks.append(Gaussian_blur(sigma=[opt.blur_sigma], is_train = is_train))
         attacks.append(Gaussian_noise([opt.noise_sigma], is_train))
-        attacks.append(DiffJPEG(height=opt.image_size, width=opt.image_size, differentiable=is_train, quality=opt.jpeg_quality))
+        attacks.append(Jpeg(is_train, opt.jpeg_quality, opt.img_size).to(opt.device))
 
         attack = Combination_attack(attacks, is_train)
 
